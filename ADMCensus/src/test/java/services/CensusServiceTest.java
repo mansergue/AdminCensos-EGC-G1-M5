5aadb809ecbf903563acb85290e26b36d760c6fb
Ismael <heluani@hotmail.com>
2015-12-02 18:36:05 +0100

    Limpieza de imports de la clase CensusServiceTest.

--- ADMCensus/src/test/java/services/CensusServiceTest.java
+++ ADMCensus/src/test/java/services/CensusServiceTest.java
@@ -1,23 +1,15 @@
 package services;
 
 import java.text.ParseException;
-import java.util.Date;
 import java.util.HashMap;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.TestingAuthenticationToken;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import security.LoginService;
 import services.CensusService;
-import utilities.PopulateDatabase;
 import domain.Census;
 
 @ContextConfiguration(locations = { "classpath:spring/datasource.xml", 
