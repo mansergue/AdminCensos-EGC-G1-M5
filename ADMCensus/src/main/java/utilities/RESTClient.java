0f91822ae181532d98668eafb44ecb13dde2d24d
Jorge Roncel <jorgeron1993@hotmail.com>
2015-12-02 12:50:28 +0100

    Actualizado RESTClient
    Actualizado el método para leer el JSON de autenticación

--- ADMCensus/src/main/java/utilities/RESTClient.java
+++ ADMCensus/src/main/java/utilities/RESTClient.java
@@ -7,50 +7,35 @@ import java.util.Collection;
 import org.springframework.web.client.RestTemplate;
 
 public class RESTClient {
-	
+
 	/***
 	 * 
-	 * Metodo que le un Json de autenticacion y devuelve una lista con los nombres de usuario para que el
-	 * admin del censo pueda añadir usuarios. 
+	 * Metodo que le un Json de autenticacion y devuelve una lista con los
+	 * nombres de usuario para que el admin del censo pueda añadir usuarios.
 	 * 
 	 * @return
 	 */
-	public static Collection<String> getListUsernamesByJsonAutentication(){
+	public static Collection<String> getListUsernamesByJsonAutentication() {
 		RestTemplate restTemplate = new RestTemplate();
 		String result = restTemplate.getForObject("http://auth-egc.azurewebsites.net/api/getUsers", String.class);
 		String[] lista = result.split(",");
-		
+
 		Collection<String> usernames = new ArrayList<String>();
-		for(int i=0; i<lista.length; i++){
-			String aux = lista[i];
-			String aux2 = "";
-			if(i==0){
-				aux = aux.replace("[{\"username\":\"", "");
-				aux = aux.replace("\"", "");
-				aux = aux.replace("ï»¿", "");
-				aux2 = aux;
-				
-			}else{
-				if(aux.contains("username")){
-					aux = aux.replace("{\"username\":\"", "");
-					aux = aux.replace("\"", "");
-					aux2 = aux;
-				}
+		for (String field : lista) {
+			if (field.contains("username")) {
+				String[] aux_list = field.split(":");
+				String username = aux_list[1];
+				username = username.replaceAll("\"", "");
+				usernames.add(username);
 			}
-			if(!aux2.equals("")){
-				aux2 = aux2.replace(" ", "");
-				usernames.add(aux2);
-			}
-			
 		}
-		
+
 		return usernames;
 	}
 
-	
 	public static void main(String[] args) throws IOException {
 		Collection<String> usernames = getListUsernamesByJsonAutentication();
-		for(String st: usernames){
+		for (String st : usernames) {
 			System.out.println(st);
 		}
 	}
