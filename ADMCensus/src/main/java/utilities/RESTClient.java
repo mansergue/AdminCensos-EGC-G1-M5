977a5432beb3a2900e16ff99db36704061470d7f
susmarmar <susikawa@hotmail.com>
2015-12-09 16:27:07 +0100

    Json
    Se cambia el parseo del jason para que devuelva además de los usuarios,
    sus emails para poder ser notificados de los cambios en el censo. Esta
    clase, entonces, devuelve un map con los usuarios como clave y sus
    emails como valor.

--- ADMCensus/src/main/java/utilities/RESTClient.java
+++ ADMCensus/src/main/java/utilities/RESTClient.java
@@ -1,42 +1,68 @@
 package utilities;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.web.client.RestTemplate;
 
 public class RESTClient {
 
+	
+	
 	/***
 	 * 
-	 * Método que lee un Json de autenticación y devuelve una lista con los
-	 * username de todos los usuarios registrados en el sistema para que el
-	 * admin del censo pueda añadir usuarios.
+	 * Método que lee un Json de autenticación y devuelve un map con los
+	 * username de todos los usuarios registrados en el sistema como clave y
+	 * sus emails como valor para que el
+	 * admin del censo pueda añadir usuarios y se les notifique mediante email.
 	 * 
 	 */
-	public static Collection<String> getListUsernamesByJsonAutentication() {
+	
+	
+	
+	public static Map<String,String> getMapUSernameAndEmailByJsonAutentication() {
+
 		RestTemplate restTemplate = new RestTemplate();
 		String result = restTemplate.getForObject("http://auth-egc.azurewebsites.net/api/getUsers", String.class);
-		String[] lista = result.split(",");
+		String[] lista = result.split("},");
+		
+		Map<String,String> mapUsernamesEmails = new HashMap<String,String>();
+		String username = null;
+		String email;
+		for (@SuppressWarnings("unused") String usuario : lista) {
+			
+			String[] lista2 = result.split(",");
+			for(String campo : lista2){
+				String[] aux_list = campo.split(":");
+				if(campo.contains("username")){
+					username = aux_list[1];
+					username = username.replaceAll("\"", "");
+					mapUsernamesEmails.put(username, null);
 
-		Collection<String> usernames = new ArrayList<String>();
-		for (String field : lista) {
-			if (field.contains("username")) {
-				String[] aux_list = field.split(":");
-				String username = aux_list[1];
-				username = username.replaceAll("\"", "");
-				usernames.add(username);
-			}
-		}
+				}
+				if(campo.contains("email")){
+					email = aux_list[1];
+					email = email.replaceAll("\"", "");
+					mapUsernamesEmails.put(username, email);
+				}
+				
+		   }
+				
+	   }
 
-		return usernames;
+		return mapUsernamesEmails;
 	}
+	
+
 
 	public static void main(String[] args) throws IOException {
-		Collection<String> usernames = getListUsernamesByJsonAutentication();
-		for (String st : usernames) {
-			System.out.println(st);
+		Map<String, String> usernamesAndEmails = getMapUSernameAndEmailByJsonAutentication();
+			System.out.println(usernamesAndEmails);
 		}
-	}
+	
+
+
 }
+
