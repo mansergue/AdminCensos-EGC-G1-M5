1767833e05aa4978ef81a59496666fc95521ceac
Carlos <carlos.tore.95@hotmail.es>
2018-01-10 12:04:47 +0100

    Modificación de modelo

--- principal/forms.py
+++ principal/forms.py
@@ -5,4 +5,11 @@ class CensusForm(ModelForm):
     class Meta:
         model = Census
         fields = ['title','postalCode','ca']
-
+class UserAccount(modelForm):
+    class Meta:
+        model = UserAccount
+        fields = ['username','password','email','role']
+class UserAccountPerCensus(modelForm):
+    class Meta:
+        model = UserAccountPerCensus
+        fields = ['census','user_account']
